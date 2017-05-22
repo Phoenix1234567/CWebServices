752e4dcc8e171164156e81ec54b585d128358324
vkumar <vkumar@dexcom.com>
2017-05-22 17:24:30 +0530

    structure refactored

---
+++ src/main/java/com/india/rest/dao/DataDao.java
@@ -0,0 +1,16 @@
+package com.india.rest.dao;
+
+import java.util.List;
+
+import com.india.rest.model.Employee;
+
+public interface DataDao {
+
+	public boolean addEntity(Employee employee) throws Exception;
+
+	public Employee getEntityById(long id) throws Exception;
+
+	public List<Employee> getEntityList() throws Exception;
+
+	public boolean deleteEntity(long id) throws Exception;
+}

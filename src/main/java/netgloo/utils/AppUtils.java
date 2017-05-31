adcf98c517dc8cd3f07fab5751c8876fc1225b59
vkumar <vkumar@dexcom.com>
2017-05-31 14:54:18 +0530

    Added ServiceUrls.docx file

--- src/main/java/netgloo/utils/AppUtils.java
+++ src/main/java/netgloo/utils/AppUtils.java
@@ -1,10 +1,8 @@
 package netgloo.utils;
 
-import java.util.UUID;
-
 public class AppUtils {
 
-	public String uuidToString(UUID uuid) {
+	/*public String uuidToString(UUID uuid) {
 		return uuid.toString();
-	}
+	}*/
 }
